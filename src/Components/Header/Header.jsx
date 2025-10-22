84c61cce8f390a59c7bc51e38c0876019050b71a
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-10-21 23:56:55 -0700

    Updated Components folder

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,1 +1,17 @@
 import './Header.css'
+import Navbar from './Navbar/Navbar'
+
+import React from 'react'
+
+const Header = () => {
+  return ( 
+    <>
+       <img src="../src/assets/LLE logo 1.png" alt="" />
+    <Navbar/>
+    </>
+   
+  )
+}
+
+export default Header
+
---
+++ src/Components/Header/Navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+import './Navbar.css'
+
+
+const Navbar = () => {
+    return (
+        <>
+        
+<div>
+   <ul className='lists'>
+    <li className='list'>Home</li>
+    <li>About us</li>
+    <li>Events</li>
+    <li>Contact us</li>
+    </ul>
+<button>Donate</button>
+</div>
+
+         
+        </>
+    )
+}  
+
+export default Navbar
