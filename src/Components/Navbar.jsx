0d00022e0be2441fe3a8af658e745a55e90d0e79
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-10-30 15:23:12 +0100

    added hover text color to the li

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,13 +3,13 @@ import Logo from "../assets/LLE logo 1.png";
 const Navbar = () => {
   return (
     <div className="w-full text-center ">
-      <div className="bg-blue-50 flex justify-between text-[#9999] font-medium p-4 gap-7 ">
+      <div className="bg-amber-400 flex justify-between text-[#9999] font-medium p-4 gap-7 ">
         <img src={Logo} alt="The logo image" width={99} />
         <ul className="flex text-center text-2xl">
           <li className="p-4 text-[#A60000] hover:text-[#fd0707]">Home</li>
-          <li className="p-4 hover:text-[#fd0707] " >About us</li>
-          <li className="p-4 hover:text-[#fd0707] " >Events</li>
-          <li className="p-4 hover:text-[#fd0707]" >Contact us</li>
+          <li className="p-4 hover:text-[#fd0707] ">About us</li>
+          <li className="p-4 hover:text-[#fd0707] ">Events</li>
+          <li className="p-4 hover:text-[#fd0707]">Contact us</li>
         </ul>
         <button className="bg-black w-[125px] rounded-md text-white m-2 shadow-[#A60000] hover:bg-[#4C0101] ease-out duration-300 ">Donate</button>
       </div>
