7e45f26be14d0f93852c213d5827077fc8376404
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-11-03 17:35:58 +0100

    added pictures

--- src/Components/hero.jsx
+++ src/Components/hero.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import Frame from '../assets/Frame 13.png'
+import Frame15 from '../assets/Frame 15.png'
 const Hero = () => {
   return (
-    <div>v
-      img
-      <img src={Frame} alt="The frame picture" />
-
+    <div className='bg-cover h-screen bg-center text-center flex flex-col py-16 '  style={{ backgroundImage: `url(${Frame15})` }}>
+    <p className=' rounded-md p-7 bg-amber-500'>Welcome to Limted Leadership Eversion (LLE)</p>
+   
     </div>
   )
 }
