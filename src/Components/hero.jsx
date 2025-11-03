5d961085abcbf6762e56fb051486aaeb726bc1f7
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-11-03 22:02:00 +0100

    adusted fonts

--- src/Components/hero.jsx
+++ src/Components/hero.jsx
@@ -4,12 +4,12 @@ import Frame15 from '../assets/Frame 15.png'
 const Hero = () => {
   return (
     <div className='bg-cover h-screen gap-10 bg-center text-center flex justify-center flex-col py-16 '  style={{ backgroundImage: `url(${Frame15})` }}>
-    <div className='w-90 flex items-center mx-auto'>    
-      <p className='text-[#A60000] rounded-full bg-[#F6E6E6] p-2'>Welcome to Limted Leadership Eversion (LLE)</p>
+    <div className='w-90 font-montserrat flex items-center mx-auto '>    
+      <p className='text-[#A60000] rounded-full text-xl mb-17 bg-[#F6E6E6] p-2'>Welcome to Limted Leadership Eversion (LLE)</p>
  </div>
-    <h1 className=' text-[rgb(58,0,0)] font-montserrat font- text-7xl md:text-9xl font-medium mb-4 leading-tight  '>Where Young <br /> Leaders are built </h1>
-    <p className='text-lg md:text-xl text-[#999999] mb-8 '>A non-profit organisation aimed to equip the next generation with the character, compontence, and courage to drive growth and transform communites.</p>
-
+    <h1 className=' text-[rgb(58,0,0)] font-montserrat font-black text-7xl md:text-9xl mb-4 leading-tight  '>Where Young <br /> Leaders are built </h1>
+    <p className='text-lg md:text-xl text-[#999999] mb-8'>A non-profit organisation aimed to equip the next generation with the character, compontence, and courage to drive growth and transform communites.</p>
+       <button className='bg-[#a60000] text-white p-4 mx-auto rounded-lg hover:bg-[#3a0000] '>Partner with us</button>
     </div>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
   )
 }
