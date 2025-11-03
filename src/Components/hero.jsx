c01ff55e4b03e8a33750ece8d8e9c953623104c5
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-11-03 19:14:02 +0100

    added h1 and p to the hero section

--- src/Components/hero.jsx
+++ src/Components/hero.jsx
@@ -3,10 +3,11 @@ import Frame from '../assets/Frame 13.png'
 import Frame15 from '../assets/Frame 15.png'
 const Hero = () => {
   return (
-    <div className='bg-cover h-screen bg-center text-center flex flex-col py-16 '  style={{ backgroundImage: `url(${Frame15})` }}>
-    <p className=' rounded-md p-7 bg-amber-500'>Welcome to Limted Leadership Eversion (LLE)</p>
-   
-    </div>
+    <div className='bg-cover h-screen gap-80 bg-center text-center flex-col py-16 '  style={{ backgroundImage: `url(${Frame15})` }}>
+    <p className='text-[#A60000] rounded-full bg-[#F6E6E6] ml-50 mr-50 p-2'>Welcome to Limted Leadership Eversion (LLE)</p>
+    <h1 className=' text-[#3a0000] text-9xl font-medium'>Where Young Leaders are built </h1>
+
+    </div>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
   )
 }
 
