f87607b46cea1e5f4f4b2f12d5bc4dadbcafdbd0
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-11-04 23:59:19 +0100

    updated event file

--- src/Components/event.jsx
+++ src/Components/event.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const event = () => {
   return (
-    <div className='flex flex-col font-Montserrat text-[#999] bg-black py-16 justify-center  '>
-    <div>
+    <div className='flex flex-col items-center text-center font-Montserrat text-[#999] bg-black py-16 justify-center  '>
+    <div className='flex flex-col '>
      <h5 className='text-[#ff6300] font-medium text-lg'>Event</h5>
-     <h2 className='font-extralight font-2xl'>Upcoming Event</h2>
+     <h2 className='font-bold font-2xl'>Upcoming Event</h2>
    </div>
 
     </div>
