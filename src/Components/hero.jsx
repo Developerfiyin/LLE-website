82ca7bd2cdb123486f13b6a2e22ca14436991259
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-11-04 17:16:20 +0100

    found error in animation loop

--- src/Components/hero.jsx
+++ src/Components/hero.jsx
@@ -7,16 +7,15 @@ import Frame15 from '../assets/Frame 15.png'
 const Hero = () => {
   
   return (
-    <div className='bg-cover h-screen relative gap-10 bg-center text-center flex justify-center flex-col py-16 '  style={{ backgroundImage: `url(${Frame15})` }}>
-    <div className='w-90 font-montserrat flex text-center mt-100 '>    
-      <p className='text-[#A60000] rounded-full text-xl border-t border-grey-800 bg-[#F6E6E6] p-2'>Welcome to Limted Leadership Eversion (LLE)</p>
+    <div className='bg-cover h-screen relative gap-10 bg-center text-center flex justify-center flex-col py-16 px-4'  style={{ backgroundImage: `url(${Frame15})` }}>
+    <div className='w-90 font-montserrat flex text-center mx-auto'>    
+      <p className='mt-90 text-[#A60000] rounded-full text-xl border-t border-grey-800 bg-[#F6E6E6] p-2'>Welcome to Limted Leadership Eversion (LLE)</p>
  </div>
     <h1 className='text-[hsl(0,100%,11%)] font-montserrat font-black text-7xl md:text-9xl mb-4 leading-tight'>Where Young <br /> Leaders are built </h1>
     <p className='text-lg md:text-xl text-[#999999] mb-8'>A non-profit organisation aimed to equip the next generation with the character, compontence, and courage to drive growth and transform communites.</p>
        <button className='bg-[#a60000] text-white p-4 mx-auto rounded-lg hover:bg-[#3a0000] '>Partner with us</button>
       
       <div className='flex overflow-hidden space-x-16'>
-
            <div className='flex justify-center gap-4 animate-loop-scroll'>
          <img src={img1} alt="The rectangle image from figma" className='max-w-none' />
          <img src={img2} alt="The rectangle image from figma" className='max-w-none' />
@@ -30,7 +29,6 @@ const Hero = () => {
          <img src={img3} alt="photo of the student from the figma design" />
          <img src={img4} alt="photo of the audience fromt the figma design" />
        </div>
-
       </div>
          
      
