0cfd4f4c36f1933ed94b34dc2d27895bc40de3fd
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-11-05 07:45:46 +0100

    Added location image

--- src/Components/event.jsx
+++ src/Components/event.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
+import Frame from '../assets/Rectangle 22.png'
+
 
 const event = () => {
   return (
-    <div className='flex flex-col items-center text-center font-Montserrat text-[#999] bg-black py-16 justify-center  '>
-    <div className='flex flex-col '>
-     <h5 className='text-[#ff6300] font-medium text-lg'>Event</h5>
-     <h2 className='font-bold font-2xl'>Upcoming Event</h2>
+    <div className='flex flex-col p-4 items-center text-center font-Montserrat text-[#999] bg-black py-16 justify-center'>
+    <div className='flex flex-col mb-4'>
+     <h5 className='text-[#ff6300] font-medium text-2xl'>Event</h5>
+     <h2 className='font-bold text-5xl text-[#3a0000]'>Upcoming Event</h2>
    </div>
 
+     <div className='w-150 bg-white flex rounded-lg py-12 p-2 shadow-green-700'> 
+          <img src={Frame} alt="" sizes='40' className='bg-white rounded-md p-19'/>
+      <h4 className='text-[#3a0000] font-medium text-3xl '>ILE-IFE LEADERSHIP SYMPOSUIM</h4>
+      <img src="" alt="" />
+      <span></span>
+
+     </div>
     </div>
   )
 }   
--- src/Components/hero.jsx
+++ src/Components/hero.jsx
@@ -3,15 +3,15 @@ import img1 from '../assets/Rectangle 21.png'
 import img2 from '../assets/Rectangle 22.png'
 import img3 from '../assets/Rectangle 23.png'
 import img4 from '../assets/Rectangle 24.png'
-import Frame15 from '../assets/Frame 15.png'
+import Group from '../assets/Group.png'
 const Hero = () => {
   
   return (
-    <div className='bg-cover h-[70%] relative gap-10 bg-center text-center flex justify-center flex-col py-16 px-4'  style={{ backgroundImage: `url(${Frame15})` }}>
+    <div className='bg-cover h-[70%] relative gap-10 bg-center text-center flex justify-center flex-col py-16 px-4'  style={{ backgroundImage: `url(${Group})` }}>
     <div className='w-90 font-montserrat flex text-center mx-auto'>    
-      <p className='mt-90 text-[#A60000] rounded-full text-xl border-t border-grey-800 bg-[#F6E6E6] p-2'>Welcome to Limted Leadership Eversion (LLE)</p>
- </div>
-    <h1 className='text-[hsl(0,100%,11%)] font-montserrat font-black text-7xl md:text-9xl mb-4 leading-tight'>Where Young <br /> Leaders are built </h1>
+      <p className=' text-[#A60000] rounded-full text-xl border-t border-grey-800 bg-[#F6E6E6] p-2'>Welcome to Limted Leadership Eversion (LLE)</p>
+     </div>
+    <h1 className='text-[#3a0000] font-montserrat font-black text-7xl md:text-9xl mb-4 leading-tight'>Where Young <br /> Leaders are built </h1>
     <p className='text-lg md:text-xl text-[#999999] mb-8'>A non-profit organisation aimed to equip the next generation with the character, compontence, and courage to drive growth and transform communites.</p>
        <button className='bg-[#a60000] text-white p-4 mx-auto rounded-lg hover:bg-[#3a0000] '>Partner with us</button>
       
