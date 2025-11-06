60269ac28f6e378fdc55850cee6ccc55c1046015
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-11-06 23:40:55 +0100

    styled h2 and p tag

--- src/Components/vision.jsx
+++ src/Components/vision.jsx
@@ -5,8 +5,8 @@ const vision = () => {
     <div className='py-12 bg-[#f9f9f9] p-4 flex flex-col'>
         
           <div className='flex flex-col justify-center items-center'>
-              <p>How we work</p>
-              <h2>Building Leaders for the leaders.</h2>
+              <p className='bg-[#ffe8d9] text-[#ff6300] rounded-md p-2'>How we work</p>
+              <h2 className='bg-white text-3xl font-medium text-[#3a0000]'>Building Leaders for the future.</h2>
           </div>
         
         </div>
