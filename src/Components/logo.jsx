74eae927323997b01382bdb8cfd1158e60b06080
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-11-06 21:37:04 +0100

    a div for the texts and styled it

--- src/Components/logo.jsx
+++ src/Components/logo.jsx
@@ -2,7 +2,27 @@ import React from 'react'
 
 const logo = () => {
   return (
-    <div>logo</div>
+    <div className='h-127 bg-white py-8 p-4'>
+        <div className='flex  justify-between'>
+        <div className=' text-center bg-[#fafafa] shadow-lg p-4'>
+            <h5 className='text-[#3a0000]'>Sponsor Logo</h5>
+         </div>
+          <div className=' text-center shadow-lg'>
+            <h5 className='text-[#3a0000] bg-white '>Sponsor Logo</h5>
+         </div>
+           <div className=' text-center shadow-lg'>
+            <h5 className='text-[#3a0000] bg-white '>Sponsor Logo</h5>
+         </div>
+           <div className=' text-center shadow-lg'>
+            <h5 className='text-[#3a0000] bg-white '>Sponsor Logo</h5>
+         </div>
+           <div className=' text-center shadow-lg'>
+            <h5 className='text-[#3a0000] bg-white '>Sponsor Logo</h5>
+         </div>
+        </div>
+ 
+         
+    </div>
   )
 }
 
