a50e02473c946a10ff1e6bf29a4c7aeb1d9187f2
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-11-06 23:35:09 +0100

    Added a div to the  vision file

--- src/Components/vision.jsx
+++ src/Components/vision.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const vision = () => {
   return (
-    <div>vision</div>
+    <div className='py-12 bg-[#f9f9f9] p-4 flex flex-col'>
+        
+          <div className='flex flex-col justify-center items-center'>
+              <p>How we work</p>
+              <h2>Building Leaders for the leaders.</h2>
+          </div>
+        
+        </div>
   )
 }
 
