6956368b77f7d0a95149df25310324064e72e698
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-11-06 23:50:57 +0100

    updated vision

--- src/Components/vision.jsx
+++ src/Components/vision.jsx
@@ -5,8 +5,20 @@ const vision = () => {
     <div className='py-12 bg-[#f9f9f9] p-4 flex flex-col'>
         
           <div className='flex flex-col justify-center items-center'>
-              <p className='bg-[#ffe8d9] text-[#ff6300] rounded-md p-2'>How we work</p>
-              <h2 className='bg-white text-3xl font-medium text-[#3a0000]'>Building Leaders for the future.</h2>
+              <p className='bg-[#ffe8d9] text-[#ff6300] rounded-md p-2 mb-2'>How we work</p>
+              <h2 className='text-3xl font-medium text-[#3a0000]'>Building Leaders for the future.</h2>
+
+                       
+                       <div className='grid grid-cols-3'>
+                            
+                        <div>
+                            <h5 className='text-[#3a0000] font-semibold '>Who are you ?</h5>
+
+                        </div>
+
+                       </div>
+
+
           </div>
         
         </div>
