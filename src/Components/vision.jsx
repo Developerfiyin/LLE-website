6dc1ee37ab2b985ae25f6066bd4e8cc94d589fd8
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-11-07 21:21:49 +0100

    styled div with grid

--- src/Components/vision.jsx
+++ src/Components/vision.jsx
@@ -6,14 +6,14 @@ const vision = () => {
   return (
     <div className='py-12 bg-[#f9f9f9] p-4 flex flex-col'>
         
-          <div className='flex flex-col justify-center items-center '>
+          <div className='flex flex-col justify-evenly items-center '>
               <p className='bg-[#ffe8d9] text-[#ff6300] rounded-md p-2 mb-2'>How we work</p>
               <h2 className='text-3xl font-medium text-[#3a0000] mb-6'>Building Leaders for the future.</h2>
 
                        
-                       <div className='grid grid-cols-3'>
-                         <div className=' p-5  text-center' >
-                          <div className='bg-[#f2f2f7] flex flex-col lg:grid-cols-3 items-center gap-7 p-5 '>
+                       <div className='grid md:grid-cols-3 ml-10 rounded-lg'>
+                         <div className='p-14' >
+                          <div className='bg-[#f2f2f7] flex flex-col items-center gap-7 p-5 text-center'>
                            <img src={Frame43} alt="THE FRAME FOR THE VISION (ORANGE)" width={50}  />
                          <div className='m-2'>
                          <h5 className='text-[#0a0900] font-semibold text-4xl'>Who are you ?</h5>
@@ -27,7 +27,7 @@ const vision = () => {
                           </div>
                         
 
-
+                              <div className='p-14'>
                          <div className='bg-[#f2f2f7] flex flex-col p-5 gap-7 items-center text-center' >
                          <img src={Frame43} alt="THE FRAME FOR THE VISION (ORANGE)" width={50}  />
                          <div className='m-2'>
@@ -39,8 +39,9 @@ const vision = () => {
                         <img src={linksquare} alt="THE LINK SQUARE IMAGE" className='size-4' />
                         </div>
                          </div> 
+                           </div>
 
-
+                            <div className='p-14'>
                          <div className='bg-[#f2f2f7] flex flex-col p-5 gap-7 items-center text-center' >
                          <img src={Frame43} alt="THE FRAME FOR THE VISION (ORANGE)" width={50}  />
                          <div className='m-2'>
@@ -52,6 +53,7 @@ const vision = () => {
                         <img src={linksquare} alt="THE LINK SQUARE IMAGE" className='size-4' />
                         </div>
                          </div> 
+                       </div>
                        </div>
           </div>
         
