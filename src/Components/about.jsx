78ede5aaca1b69c3b2104bf3a3c9fe831d5a0413
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-11-08 23:56:55 +0100

    updated p tag

--- src/Components/about.jsx
+++ src/Components/about.jsx
@@ -5,13 +5,13 @@ const about = () => {
     <section className='bg-white py-16 p-4'>
 
           <div className='flex justify-evenly '>
-          <div className='mx-12'>
+          <div className='m-'>
          <img src={Frame26} alt="the picture template" />
       </div>
 
         <div className='flex flex-col'>
-       <p className='bg-[#ffe8d9] text-[#ff6300] rounded-md mx-auto p-2 mb-2'>How we work</p>
-           <h2 className='text-[#3a0000] font-bold text-3xl '>Raising Leaders, One At A Time. </h2>
+       <p className='bg-[#ffe8d9] text-[#ff6300] rounded-md mx-auto p-2 ml-0'>How we work</p>
+           <h2 className='text-[#3a0000] font-semibold text-4xl '>Raising Leaders, One At A Time. </h2>
 
 
       </div>
