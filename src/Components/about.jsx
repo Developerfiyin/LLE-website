b1246bb281b00a06158c35ce1fba18e88080c73f
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-11-08 23:59:48 +0100

    updated h2

--- src/Components/about.jsx
+++ src/Components/about.jsx
@@ -12,7 +12,7 @@ const about = () => {
         <div className='flex flex-col'>
        <p className='bg-[#ffe8d9] text-[#ff6300] rounded-md mx-auto p-2 ml-0'>How we work</p>
            <h2 className='text-[#3a0000] font-semibold text-4xl '>Raising Leaders, One At A Time. </h2>
-
+         <p>Limitless Leadership Eversion (LLE) is  a non-profit organization dedicated  to developing young adults into  capable, effective, and efficient  leaders across all social systems.</p>
 
       </div>
           </div>
