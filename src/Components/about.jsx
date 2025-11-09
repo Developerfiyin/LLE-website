80ec9cc68b97e0a55401c872f58203a80918fc4e
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-11-09 23:05:44 +0100

    updated about section

--- src/Components/about.jsx
+++ src/Components/about.jsx
@@ -4,15 +4,17 @@ const about = () => {
   return (
     <section className='bg-white py-16 p-4'>
 
-          <div className='flex justify-evenly '>
-          <div className='m-'>
+        <div className='flex justify-between '>
+          <div className='mx-10'>
          <img src={Frame26} alt="the picture template" />
       </div>
 
-        <div className='flex flex-col'>
-       <p className='bg-[#ffe8d9] text-[#ff6300] rounded-md mx-auto p-2 ml-0'>How we work</p>
-           <h2 className='text-[#3a0000] font-semibold text-4xl '>Raising Leaders, One At A Time. </h2>
-         <p>Limitless Leadership Eversion (LLE) is  a non-profit organization dedicated  to developing young adults into  capable, effective, and efficient  leaders across all social systems.</p>
+      <div className='flex flex-col mx-12 gap-2 '>
+       <p className='bg-[#ffe8d9] text-[#ff6300] rounded-md mx-auto  p-2 ml-0'>How we work</p>
+           <h2 className='text-[#3a0000] font-semibold text-4xl mb-6 '>Raising Leaders, One At A Time. </h2>
+         <p className='text-[#3a0000] border-l-4 font-medium pl-4 h-17 border-l-[#ff6400]'>Limitless Leadership Eversion (LLE) is  a non-profit organization  <br /> 
+         dedicated  to developing young adults into capable, effective, and <br />
+          efficient  leaders across all social systems.</p>
 
       </div>
           </div>
