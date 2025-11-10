ca5b8ea14d7345f541cc581446d594922844c42d
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-11-10 20:20:15 +0100

    updated the contact section

--- src/Components/contact.jsx
+++ src/Components/contact.jsx
@@ -1,18 +1,30 @@
 import React from 'react'
-
+import email from '../assets/email.png'
+import call from '../assets/call-02.png'
 const contact = () => {
   return (
     <section className='bg-[#F8F7F7] py-12 flex flex-col m-9'>
-      <div className='flex flex-col bg-[#800000] p-6 '>
+      <div className='flex flex-col p-6 '>
        <p className='bg-[#ffe8d9] text-[#ff6300] rounded-md mx-auto p-2 text-lg mb-3 ml-0'>Testimonals</p>
         <h2 className='text-[#3a0000] font-medium  mb-9'>Feel Free To Contact Us Anytime.</h2>
 
-        <div className='flex justify-evenly'>
+        <div className='flex gap-3'>
+       <img src={email} alt="The email icon" className='size-9' />
+       <div className=''>
+        <h6 className='font-medium text-[#3a0000] '>Chat Now</h6>
+        <p className='text-sm text-[#3a0000] '>Kinkan@gmail.com</p>
+       </div>
 
+       <img src={call} alt="The email icon" className='size-6' />
+       <div className=''>
+        <h6 className='font-medium text-[#3a0000] '>Call Us</h6>
+        <p className='text-sm text-[#3a0000] '>081666990000</p>
+       </div>
+       
         </div>
       </div>
       
-      contact</section>
+    </section>
   )
 }
 
