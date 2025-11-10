fd6c941346babe04c51c262b427e8b4d7984306a
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-11-10 19:41:33 +0100

    added email icon

--- src/Components/contact.jsx
+++ src/Components/contact.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const contact = () => {
   return (
-    <div className='bg-[#F8F7F7] py-12'>contact</div>
+    <section className='bg-[#F8F7F7] py-12 flex flex-col m-9'>
+      <div className='flex flex-col bg-[#800000] p-6 '>
+       <p className='bg-[#ffe8d9] text-[#ff6300] rounded-md mx-auto p-2 text-lg mb-3 ml-0'>Testimonals</p>
+        <h2 className='text-[#3a0000] font-medium  mb-9'>Feel Free To Contact Us Anytime.</h2>
+
+        <div className='flex justify-evenly'>
+
+        </div>
+      </div>
+      
+      contact</section>
   )
 }
 
