24384b2399cbd4c37f45db12f24b0ac27cc7e8b2
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-11-10 13:10:16 +0100

    Finished activities.jsx

--- src/Components/Activities.jsx
+++ src/Components/Activities.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Frame41 from '../assets/Frame 41.png'
+import Frame46 from '../assets/Frame 46.png'
 
 
 const Activities = () => {
   return (
-    <div>
+    <div className='bg-[#FFEFE6] py-16 p-4 flex justify-evenly '>
 
-
+   <img src={Frame46} alt="THE PICTURE FOR THE ACTIVITES" className='ml-8' />
+   <img src={Frame41} alt="THE frame for the activites ( it has a play icon in the middle)" />
     </div>
   )
 }
