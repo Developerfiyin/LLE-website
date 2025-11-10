9cce68094122c333e64ae664a558a4ea0376eebb
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-11-10 17:50:16 +0100

    added cards for testimonials

--- src/Components/Testimonials.jsx
+++ src/Components/Testimonials.jsx
@@ -9,13 +9,40 @@ const Testimonials = () => {
           <h2 className='font-medium text-5xl mb-9'>Their Stories. Our Success</h2>
         </div>
    
-            <div className='bg-white items-center mx-auto w-60 h-90'>
-                <div className='bg-[#f9f7f7] rounded-t-full mt-8 '>
-                 <img src={ellips} alt="The image ellips " className='items-center mx-auto shadow-lg '/>
+            <div className='flex'>
+             <div className='bg-[#f8f7f7] mx-auto items-center'>
+             <img src={ellips} alt="The image ellips"className='items-center rounded-t-full shadow-lg mx-auto '/>
                 </div>
-         
-            </div>
+              <div className='bg-white items-center text-center mx-auto w-120 h-90'>
+                 <h4 className='text-[#1a1a1a] text-xl font-medium pt-12'>~Babatunde O.</h4>
+                 <h6 className='text-[#1a1a1a] mb-15'>LLE Symposium</h6>
+                 <h3 className='font-medium text-xl'>“The Masterclass was the best I have <br />
+                  experienced this year. To say I enjoyed my <br />
+                   evening would be an understatement, really!!”</h3>
+               </div>
                
+               <div className='bg-[#f8f7f7] mx-auto items-center'>
+                 <img src={ellips} alt="The image ellips"className='items-center rounded-t-full shadow-lg mx-auto '/>
+                </div>
+            <div className='bg-white items-center text-center mx-auto w-120 h-90'>
+                 <h4 className='text-[#1a1a1a] text-xl font-medium pt-12'>~Babatunde O.</h4>
+                 <h6 className='text-[#1a1a1a] mb-15'>LLE Symposium</h6>
+                 <h3 className='font-medium text-xl'>“The Masterclass was the best I have <br />
+                  experienced this year. To say I enjoyed my <br />
+                   evening would be an understatement, really!!”</h3>
+            </div>
+            <div className='bg-[#f8f7f7] mx-auto items-center'>
+             <img src={ellips} alt="The image ellips"className='items-center rounded-t-full shadow-lg mx-auto '/>
+                </div>
+            <div className='bg-white items-center text-center mx-auto w-120 h-90'>
+                 <h4 className='text-[#1a1a1a] text-xl font-medium pt-12'>~Babatunde O.</h4>
+                 <h6 className='text-[#1a1a1a] mb-15'>LLE Symposium</h6>
+                 <h3 className='font-medium text-xl'>“The Masterclass was the best I have <br />
+                  experienced this year. To say I enjoyed my <br />
+                   evening would be an understatement, really!!”</h3>
+            </div>
+
+
         </div>
   )
 }
