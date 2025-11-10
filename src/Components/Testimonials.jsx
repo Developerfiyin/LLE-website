8db258000ee0624d0e00e0de72b7e47ef5a11d8c
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-11-10 13:53:26 +0100

    Updated testimonals

--- src/Components/Testimonials.jsx
+++ src/Components/Testimonials.jsx
@@ -3,8 +3,21 @@ import React from 'react'
 const Testimonials = () => {
   return (
     <div className='bg-[#f8f7f7] py-12 p-4 '>
-      <p className='bg-[#ffe8d9] text-[#ff6300] rounded-md mx-auto  p-2 ml-0'>How we work</p>
-          <h2>Their Stories, Our Success</h2>
+        utbl7nityrhgfervbn7m67oi8bu6ytr
+
+
+
+
+
+
+
+
+        <div className='flex flex-col items-center text-center'>
+        <p className='bg-[#ffe8d9] text-[#ff6300] rounded-md p-2 text-lg mb-3 ml-0'>Testimonals</p>
+          <h2 className='font-medium text-5xl'>Their Stories. Our Success</h2>
+        </div>
+   
+        
         
         </div>
   )
