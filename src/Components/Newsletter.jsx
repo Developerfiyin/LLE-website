76a6473d76b9bb23aa9f3783d6bff53a34ff8e29
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-11-13 19:08:09 +0100

    updated newsletter

--- src/Components/Newsletter.jsx
+++ src/Components/Newsletter.jsx
@@ -2,23 +2,24 @@ import React from 'react'
 
 const Newsletter = () => {
   return (
-    <div className='py-12 p-4 bg-white flex flex-col item-center '>
-       <p className='bg-[#ffe8d9] text-[#ff6300] text-xl rounded-lg mx-auto p-2 mb-3'>Our News Letter</p>
+    
 
+   
+     <div className='py-12 p-4 bg-white mx-auto flex flex-col item-center '>
+       <p className='bg-[#ffe8d9] text-[#ff6300] text-xl mx-auto rounded-lg  p-2 mb-3'>Our News Letter</p>
         <h3 className='font-medium text-5xl text-[#3a0000] mx-auto mb-5 '>Get the latest updates <br />
          delivered to your mail</h3>
-                     
-                     <div className='flex gap-3'> 
-                    <input type="text" placeholder='Enter your email address'
-                    className='pl-10 pr-3 py-2 w-90 ml-90 border border-gray-700 rounded-lg focus:ring-2 focus:ring-[#ff6300] focus:outline-none' />
-                  <button className='bg-[#a60000] text-white ml-0 mx-auto p-2 md:p-2 w-50 rounded-lg ease-in-out duration-300 hover:bg-[#3a0000]'>Register Now</button>
+         <div className='flex gap-3'> 
+         <input type="text" placeholder='Enter your email address'
+        className='pl-10 pr-3 py-2 w-90 ml-90 border border-gray-700 rounded-lg focus:ring-2 focus:ring-[#ff6300] focus:outline-none' />
+        <button className='bg-[#a60000] text-white ml-0 mx-auto p-2 md:p-2 w-50 rounded-lg ease-in-out duration-300 hover:bg-[#3a0000]'>Register Now</button>
+        </div>
 
-                     </div>
-                   
-
-
-
+        
     </div>
+    
+      
+    
   )
 }
 
