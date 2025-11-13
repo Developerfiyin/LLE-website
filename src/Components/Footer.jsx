f6437ab135f6bd66db460bbe950edfe3a7acbcef
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-11-13 13:41:23 +0100

    Updated Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -6,8 +6,8 @@ import Instagram  from '../assets/instagram.png'
 
 const Footer = () => {
   return (
-    <div className='py-12  bg-[#F8F7F7] p-8 '>
-    <div className='flex flex-col m-9 gap-3'>  
+    <div className='py-12  bg-[#F8F7F7] p-8 gap-9 grid lg:grid-cols-3'>
+    <div className='flex flex-col m-9 mt-0 gap-3'>  
      <p className='text-[#999] text-xl mb-8'>Limitless Leadership Eversion (LLE) is  a non-profit organization <br />
         dedicated to developing young adults into  capable, effective, and <br />
          efficient  leaders across all social systems. We recognize that <br />
@@ -16,13 +16,20 @@ const Footer = () => {
      pillar of  leadership, ensuring progress and  prosperity at every level.</p>
          
          <div className='flex gap-6 '>
-      <img src={button} alt="" className='w-8 h-8' />
-     <img src={twitter} alt="THE TWITTER ICON" className='w-8 h-8' />
-     <img src={Instagram} alt="THE INSTAGRAM ICON"className='w-8 h-8' />
-     <img src={button} alt="THE LINKDEIN ICON" className='w-8 h-8' />
-         </div>
-    
+      <img src={button} alt="" className='w-10 h-10' />
+     <img src={twitter} alt="THE TWITTER ICON" className='w-10 h-10' />
+     <img src={Instagram} alt="THE INSTAGRAM ICON"className='w-10 h-10' />
+     <img src={button} alt="THE LINKDEIN ICON" className='w-10 h-10' />
+        </div>
     </div>
+
+      <div className='ml-3 text-[#3a0000] p-6 font-semibold'>
+        <h5 className=' font-medium text-xl '>Quick Link</h5>
+          <h6 className='p-4'>Home</h6>
+          <h6 className='p-4'>About us</h6> 
+          <h6 className='p-4'>Events</h6>
+          <h6 className='p-4'>Contact Us</h6>
+      </div>
 
     </div>
   )
