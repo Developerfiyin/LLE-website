d66696874b9a52df959b25e368e0f4ba54336a9f
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-11-14 12:45:14 +0100

    updated testimonial section

--- src/Components/Testimonials.jsx
+++ src/Components/Testimonials.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import ellips from '../assets/Ellipse 1131.png'
 const Testimonials = () => {
   return (
-    <div className='bg-[#f8f7f7] py-12 p-2'>
+    <div className='bg-[#f8f7f7] py-12 p-2 mx-auto '>
     
-        <div className='flex flex-col items-center text-center'>
+        <div className='flex flex-col  items-center text-center'>
         <p className='bg-[#ffe8d9] text-[#ff6300] rounded-md p-2 text-lg mb-3 ml-0'>Testimonals</p>
           <h2 className='font-medium text-5xl mb-9'>Their Stories. Our Success</h2>
         </div>
