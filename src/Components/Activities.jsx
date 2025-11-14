1bac80d9133b3506d7bf7f3bd1f4882c76b81078
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-11-14 10:49:10 +0100

    updated style activities for small screen

--- src/Components/Activities.jsx
+++ src/Components/Activities.jsx
@@ -5,8 +5,8 @@ import Frame46 from '../assets/Frame 46.png'
 
 const Activities = () => {
   return (
-    <div className='bg-[#FFEFE6] py-16 p-4 flex gap-4 justify-evenly '>
-     <div className='grid'>
+    <div className='bg-[#FFEFE6] mx-auto py-16 p-4 flex gap-4'>
+     <div className='grid md:grid-cols-2'>
    <img src={Frame46} alt="THE PICTURE FOR THE ACTIVITES" className='ml-8' />
    <img src={Frame41} alt="THE frame for the activites ( it has a play icon in the middle)" />
     </div>
