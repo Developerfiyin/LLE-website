c8344cb18a6d1af0b5d087befcd24c07ed5d693d
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-11-29 00:12:40 +0100

    deleted unnecessary lines of code

--- src/Components/Newsletter.jsx
+++ src/Components/Newsletter.jsx
@@ -14,34 +14,12 @@ const Newsletter = () => {
            <input type="text" placeholder='Enter your email address'
             className='pl-7 pr-3 py-2 w-70 border border-gray-700 rounded-lg focus:ring-2 focus:ring-[#ff6300] focus:outline-none' />
         <button className='bg-[#a60000] text-white p-2 w-50 rounded-lg ease-in-out duration-300 hover:bg-[#3a0000]'>Register Now</button>
-
-
-
           </div>
-
-
            </div>
 
-
-     
       </main>
       
-    
-
-
-   
   )
 }
-  {/*<div className=' flex md:grid justify-around flex-col'>
-   
-       <p className='bg-[#ffe8d9] text-[#ff6300] text-xl mx-auto rounded-lg  p-2 mb-3'>Our News Letter</p>
-        <h3 className='font-medium text-5xl text-[#3a0000] mx-auto mb-5 '>Get the latest updates <br />
-         delivered to your mail</h3>
-         <div className='flex gap-3 mx-auto'> 
-         <input type="text" placeholder='Enter your email address'
-        className='pl-10 pr-3 py-2 w-90 ml-90 border border-gray-700 rounded-lg focus:ring-2 focus:ring-[#ff6300] focus:outline-none' />
-        <button className='bg-[#a60000] text-white ml-0 mx-auto p-2 md:p-2 w-50 rounded-lg ease-in-out duration-300 hover:bg-[#3a0000]'>Register Now</button>
-        </div>
-        
-    </div>*/}
+  
 export default Newsletter
