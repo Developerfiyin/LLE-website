50cd638f4d962d762bad9466ab0f4f2c09aa0c37
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-11-28 23:59:57 +0100

    added a div dfor the input and button

--- src/Components/Newsletter.jsx
+++ src/Components/Newsletter.jsx
@@ -9,7 +9,12 @@ const Newsletter = () => {
         <p className='bg-[#ffe8d9] text-[#ff6300] text-xl mx-auto rounded-lg  p-2 mb-3'>Our News Letter</p>
        <h3 className='font-medium text-5xl text-[#3a0000] mx-auto mb-5 '>Get the latest updates <br />
          delivered to your mail</h3>
+  
+          <div className='flex'>
+           <input type="text" placeholder='Enter your email address'
+            className='pl-10 pr-3 py-2 w-90 ml-90 border border-gray-700 rounded-lg focus:ring-2 focus:ring-[#ff6300] focus:outline-none' />
 
+          </div>
 
 
            </div>
