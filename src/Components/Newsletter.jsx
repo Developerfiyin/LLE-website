1aedaffaa0f2f49aad3389d3f0d4b6c11f8dae99
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-11-28 21:17:20 +0100

    updated newsletter

--- src/Components/Newsletter.jsx
+++ src/Components/Newsletter.jsx
@@ -3,25 +3,37 @@ import React from 'react'
 const Newsletter = () => {
   return (
     
+    <main className='py-12 p-4 bg-white items-center mx-auto'>
+          
+     
+      </main>
+      
+    
+
+
+
+
+
+
+
+
+
+
+
 
    
-     <div className='py-12 p-4 bg-white mx-auto flex flex-col item-center '>
+  )
+}
+  {/*<div className=' flex md:grid justify-around flex-col'>
+   
        <p className='bg-[#ffe8d9] text-[#ff6300] text-xl mx-auto rounded-lg  p-2 mb-3'>Our News Letter</p>
         <h3 className='font-medium text-5xl text-[#3a0000] mx-auto mb-5 '>Get the latest updates <br />
          delivered to your mail</h3>
-         <div className='flex gap-3'> 
-          
+         <div className='flex gap-3 mx-auto'> 
          <input type="text" placeholder='Enter your email address'
         className='pl-10 pr-3 py-2 w-90 ml-90 border border-gray-700 rounded-lg focus:ring-2 focus:ring-[#ff6300] focus:outline-none' />
         <button className='bg-[#a60000] text-white ml-0 mx-auto p-2 md:p-2 w-50 rounded-lg ease-in-out duration-300 hover:bg-[#3a0000]'>Register Now</button>
         </div>
-
         
-    </div>
-    
-      
-    
-  )
-}
-
+    </div>*/}
 export default Newsletter
--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -16,7 +16,7 @@ const Header = () => {
     <header className='bg-amber-50 flex justify-between text-[#9999] py-10 p-4 mx-auto text-center'>
        <img src={Logo} alt="The logo image" width={100} />
        <div className='hidden md:flex gap-8 font-medium'>
-       <a href="#hero" className='text-[#A60000] hover:text-[#fd0707]'>Home</a>
+       <a href="#home" className='text-[#A60000] hover:text-[#fd0707]'>Home</a>
        <a href="#about" className='hover:text-[#fd0707]'>About us</a>
        <a href="#event" className='hover:text-[#fd0707]'>Events</a>
        <a href="#contact" className='hover:text-[#fd0707] '>Contact us </a>
@@ -24,8 +24,8 @@ const Header = () => {
       
     <button className="bg-black w-[125px] rounded-md text-white shadow-[#A60000] hover:bg-[#4C0101] ease-out duration-300 ">Donate</button>
  
-          <div className={nav ? "gap-8 flex flex-col space-y-4 fixed w-[50%] border-b border-b-grey-900 h-full border-r top-0 left-0 bg-white text-center text-2xl ease-in-out duration-300" : 'fixed -left-full ease-in-out duration-300 ' }>
-          <a href="#hero" className="p-4 text-[#A60000] hover:text-[#fd0707]">Home</a>
+          <div className={nav ? "gap-8 flex flex-col space-y-4 fixed w-[50%] border-b border-b-grey-900 h-full border-r top-0 left-0 bg-white text-center text-2xl ease-in-out duration-300" : 'fixed -left-full ease-in-out duration-300' }>
+          <a href="#home" className="p-4 text-[#A60000] hover:text-[#fd0707]">Home</a>
           <a href="#about" className="p-4 hover:text-[#fd0707] ">About us</a>
           <a href="#event" className="p-4 hover:text-[#fd0707] ">Events</a>
           <a href="#contact" className="p-4 hover:text-[#fd0707]">Contact us</a>
