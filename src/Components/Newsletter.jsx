25e53681c7bc3b9a3b3bb1339e64cf6dc0517a88
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-11-29 00:10:06 +0100

    Updated Newsletter.jsx section

--- src/Components/Newsletter.jsx
+++ src/Components/Newsletter.jsx
@@ -10,9 +10,12 @@ const Newsletter = () => {
        <h3 className='font-medium text-5xl text-[#3a0000] mx-auto mb-5 '>Get the latest updates <br />
          delivered to your mail</h3>
   
-          <div className='flex'>
+          <div className='flex gap-3 justify-center'>
            <input type="text" placeholder='Enter your email address'
-            className='pl-10 pr-3 py-2 w-90 ml-90 border border-gray-700 rounded-lg focus:ring-2 focus:ring-[#ff6300] focus:outline-none' />
+            className='pl-7 pr-3 py-2 w-70 border border-gray-700 rounded-lg focus:ring-2 focus:ring-[#ff6300] focus:outline-none' />
+        <button className='bg-[#a60000] text-white p-2 w-50 rounded-lg ease-in-out duration-300 hover:bg-[#3a0000]'>Register Now</button>
+
+
 
           </div>
 
